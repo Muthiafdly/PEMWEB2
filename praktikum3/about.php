326d05dcdb24bfc36d063c136bd7c2b66503aded
Muthiafdly <muthiaaf85@gmail.com>
2025-03-13 21:35:06 +0700

    Add files via upload
    praktikum3

---
+++ praktikum3/about.php
@@ -0,0 +1,13 @@
+<?php include_once "template/top.php"; ?>
+<?php include_once "template/menu.php"; ?>
+<!-- Page content wrapper-->
+<div id="page-content-wrapper">
+    <?php include_once "template/navbar.php"; ?>
+    <!-- Page content-->
+    <div class="container-fluid">
+        <h1 class="mt-4 text-center">Tentang Saya</h1>
+        <img src="img/bot.png" alt="" class="w-25 d-block mx-auto rounded-circle border border-5 mt-2">
+        <p class="text-center offset-2 col-8 mt-4 fs-5">Hallo, nama saya Muthia fadli. Saya mahasiswa dari STT NURUL FIKRI dan jurusan saya SISTEM INFORMASI. Saya berusia 19 tahun dan tinggal di DEPOK. Saya tertarik dengan dunia teknologi, khususnya dalam pengembangan sistem dan analisis data. Saya juga suka belajar hal baru karna lebih menantang dan saya senang mencari solusi kreatif dalam pemrograman. Senang bisa berkenalan dengan anda!</p>
+    </div>
+</div>
+<?php include_once "template/bottom.php"; ?>
---
+++ praktikum3/index.php
@@ -0,0 +1,13 @@
+<?php include_once "template/top.php"; ?>
+<?php include_once "template/menu.php"; ?>
+<!-- Page content wrapper-->
+<div id="page-content-wrapper">
+    <?php include_once "template/navbar.php"; ?>
+    <!-- Page content-->
+    <div class="container-fluid text-center">
+        <h1 class="mt-4">Halaman Utama</h1>
+        <!-- describe the page -->
+        <p>Ini adalah halaman utama, selamat datang User !.</p>
+    </div>
+</div>
+<?php include_once "template/bottom.php"; ?>
